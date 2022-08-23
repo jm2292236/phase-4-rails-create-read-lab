c853501436b5bffed4183fe83fd06a2795103fa9
Jaime Montes de Oca <jaime0422@live.com>
2022-08-22 19:28:08 -0700

    Completed Assignment

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,27 @@
+class PlantsController < ApplicationController
+
+    def index
+        render json: Plant.all
+    end
+
+    def create
+        new_plant = Plant.create(plant_params)
+        render json: new_plant, status: :created
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: "Plant not found!"}, status: :not_found
+        end
+    end
+
+    private
+
+    def plant_params
+        params.permit(:name, :image, :price)
+    end
+
+end
---
+++ db/mysql.sql
@@ -0,0 +1,10 @@
+INSERT INTO AnotherPlant (name, image, price) 
+VALUES ("nueva planta", "url", 54.54);
+
+CREATE TABLE AnotherPlant (
+    name string,
+    image string,
+    price decimal
+);
+
+SELECT * FROM AnotherPlant;
